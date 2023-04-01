f3747d234f0d45e9c7d34baa6bedcb37a7b412dd
Elvan Hidayat <elvanhd@gmail.com>
2023-04-01 14:15:19 +0700

    create a router

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 
+const router = require('./router');
+
 const app = express();
 
 app.use(express.static('public'));
-
 app.set('views', 'views');
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-  res.render('home-guest');
-});
+app.use('/', router);
 
 app.listen(3000);
---
+++ router.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', function (req, res) {
+  res.render('home-guest');
+});
+
+router.get('/about', function (req, res) {
+  res.send('This is our about page');
+});
+
+module.exports = router;
