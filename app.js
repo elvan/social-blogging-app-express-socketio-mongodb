b0719cfc35ff319fa47681d2e5a8a2bbe25ba56d
Elvan Hidayat <elvanhd@gmail.com>
2023-04-16 03:36:24 +0700

    identifying a user request

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
+require('dotenv').config();
+
 const express = require('express');
+const session = require('express-session');
 
 const router = require('./router');
 
+const SECRET = process.env.SECRET || 'my-secret-key';
+
 const app = express();
 
+let sessionOptions = session({
+  secret: SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 1000 * 60 * 60 * 24, httpOnly: true },
+});
+
+app.use(sessionOptions);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static('public'));
