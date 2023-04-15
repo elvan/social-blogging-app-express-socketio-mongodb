4c7ab2681db20b89a1c7a60a012dab2218cbe55d
Elvan Hidayat <elvanhd@gmail.com>
2023-04-15 22:42:47 +0700

    creating a user model

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,12 @@
+const User = require('../models/User');
+
 exports.login = function () {};
 
 exports.logout = function () {};
 
 exports.register = function (req, res) {
+  let user = new User(req.body);
+  user.register();
   res.send('Thanks for trying to register');
 };
 
