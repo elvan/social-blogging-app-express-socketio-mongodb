91dcdb9cb00c7ab435cc2bd1d7bd0a569d635b1a
Elvan Hidayat <elvanhd@gmail.com>
2023-04-16 13:33:27 +0700

    letting users logout

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,14 +6,20 @@ exports.login = function (req, res) {
     .login()
     .then(function (result) {
       req.session.user = { favColor: 'blue', username: user.data.username };
-      res.send(result);
+      req.session.save(function () {
+        res.redirect('/');
+      });
     })
     .catch(function (e) {
       res.send(e);
     });
 };
 
-exports.logout = function () {};
+exports.logout = function (req, res) {
+  req.session.destroy(function () {
+    res.redirect('/');
+  });
+};
 
 exports.register = function (req, res) {
   let user = new User(req.body);
--- router.js
+++ router.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/', userController.home);
 router.post('/register', userController.register);
 router.post('/login', userController.login);
+router.post('/logout', userController.logout);
 
 module.exports = router;
