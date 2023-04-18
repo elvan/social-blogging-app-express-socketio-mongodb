263231bb8e1dcb8f30f82b650ef91b8ed94e5971
Elvan Hidayat <elvanhd@gmail.com>
2023-04-18 21:02:51 +0700

    adding live form validation

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -1,9 +1,15 @@
 import Chat from './modules/chat';
+import RegistrationForm from './modules/registrationForm';
 import Search from './modules/search';
+
+if (document.querySelector('#registration-form')) {
+  new RegistrationForm();
+}
 
 if (document.querySelector('#chat-wrapper')) {
   new Chat();
 }
+
 if (document.querySelector('.header-search-icon')) {
   new Search();
 }
