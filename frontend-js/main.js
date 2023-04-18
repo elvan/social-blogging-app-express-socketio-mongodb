aafb46cb2a0dc205aed79e2048f4fd0a167c1485
Elvan Hidayat <elvanhd@gmail.com>
2023-04-18 11:26:48 +0700

    adding widget for chat feature

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -1,5 +1,9 @@
+import Chat from './modules/chat';
 import Search from './modules/search';
 
+if (document.querySelector('#chat-wrapper')) {
+  new Chat();
+}
 if (document.querySelector('.header-search-icon')) {
   new Search();
 }
