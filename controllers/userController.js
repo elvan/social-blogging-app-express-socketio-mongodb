2a177427ec2a7a9c1da1b0bf3ca41c5aba440ad8
Elvan Hidayat <elvanhd@gmail.com>
2023-04-19 19:45:51 +0700

    adding cors and finishing the api

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,6 +6,16 @@ const User = require('../models/User');
 
 const JWTSECRET = process.env.JWTSECRET || 'secret';
 
+exports.apiGetPostsByUsername = async function (req, res) {
+  try {
+    let authorDoc = await User.findByUsername(req.params.username);
+    let posts = await Post.findByAuthorId(authorDoc._id);
+    res.json(posts);
+  } catch {
+    res.json('Sorry, invalid user requested.');
+  }
+};
+
 exports.apiMustBeLoggedIn = function (req, res, next) {
   try {
     req.apiUser = jwt.verify(req.body.token, JWTSECRET);
--- router-api.js
+++ router-api.js
@@ -1,9 +1,14 @@
+const cors = require('cors');
 const apiRouter = require('express').Router();
-const userController = require('./controllers/userController');
+
 const postController = require('./controllers/postController');
-const followController = require('./controllers/followController');
+const userController = require('./controllers/userController');
+
+apiRouter.use(cors());
 
 apiRouter.post('/login', userController.apiLogin);
 apiRouter.post('/create-post', userController.apiMustBeLoggedIn, postController.apiCreate);
+apiRouter.delete('/post/:id', userController.apiMustBeLoggedIn, postController.apiDelete);
+apiRouter.get('/postsByAuthor/:username', userController.apiGetPostsByUsername);
 
 module.exports = apiRouter;
